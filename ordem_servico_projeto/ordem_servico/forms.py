3dec76e73caeca1136742fac0f62210f58491437
Brakister <smashbored@gmail.com>
2024-05-08 03:58:48 -0300

    db

--- ordem_servico_projeto/ordem_servico/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Loja, Servico
-
-class OrdemServicoForm(forms.Form):
-    loja = forms.ModelChoiceField(queryset=Loja.objects.all())
-    servicos = forms.ModelMultipleChoiceField(queryset=Servico.objects.all())
