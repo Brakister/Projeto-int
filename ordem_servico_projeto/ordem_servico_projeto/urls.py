7af2e3ed1d9fc6d6adb7a46e440a1d78cfd2744b
Brakister <smashbored@gmail.com>
2024-05-08 00:54:15 -0300

    DB com OS simplificada

--- ordem_servico_projeto/ordem_servico_projeto/urls.py
+++ ordem_servico_projeto/ordem_servico_projeto/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import RedirectView
 from ordem_servico import views as ordem_servico_views
 
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='criar_ordem_servico'), name='index'),
     path('criar_ordem_servico/', ordem_servico_views.criar_ordem_servico, name='criar_ordem_servico'),
+    path('admin/', admin.site.urls),  # Adiciona as URLs do painel de administração
     # Adicione outras URLs conforme necessário
 ]
